ae888bfd2160d7a9903e575ea6fe1075b8119322
Brandon Caplan <bcaplan@onehub.com>
2009-05-07 14:38:32 -0700

    Started testing resolve and caching

--- lib/apache_lookup.rb
+++ lib/apache_lookup.rb
@@ -1,3 +1,6 @@
+require 'resolv'
+require 'yaml'
+
 class ApacheLookup
   VERSION = '1.0.0'
 end
--- test/test_apache_lookup.rb
+++ test/test_apache_lookup.rb
@@ -2,7 +2,47 @@ require "test/unit"
 require "apache_lookup"
 
 class TestApacheLookup < Test::Unit::TestCase
-  def test_sanity
-    flunk "write tests or I will kneecap you"
+  def setup
+    @test_log = StringIO.new(File.read('./test/test_log.txt'))
+    @cache = YAML.load_file('./test/cache.yml')
+    # @test_log.each_line {|line| puts "#{@test_log.lineno}: #{line}"}
+  end
+  
+  def test_pulls_from_cache_if_in_cache_and_not_expired
+    actual = ApacheLookup.resolve_ip '1.1.1.1'
+    
+    assert_equal '1111.com', actual
+  end
+  
+  def test_resolves_ip_if_not_in_cache
+    actual = ApacheLookup.resolve_ip '1.1.1.2'
+    
+    assert_equal '1112.com', actual
+  end
+  
+  def test_resolves_ip_if_expired
+    actual = ApacheLookup.resolve_ip '1.1.1.3'
+    
+    assert_equal '1113.com', actual
+  end
+  
+  def test_caches_ip_if_expired
+    actual = ApacheLookup.resolve_ip '1.1.1.4'
+    
+    assert_equal '1114.com', actual
+  end
+  
+  def test_caches_ip_if_not_in_cache
+    actual = ApacheLookup.resolve_ip '1.1.1.5'
+    
+    assert_equal '1115.com', actual
+  end
+  
+  def test_finds_and_stores_ips_and_lines
+    expected = { '208.77.188.166' => [0, 4, 7], '75.119.201.189' => []}
+    actual = ApacheLookup.gather @test_log
+    
+    
+    assert_equal expected, actual
   end
 end
