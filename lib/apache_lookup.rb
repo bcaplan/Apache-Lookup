afb018afa0570acd5c36d29d812c1fae3449f337
Brandon Caplan <bcaplan@onehub.com>
2009-05-07 20:55:33 -0700

    Cleaned up trailing whitespace for Ryan ;-). Added test to make sure mtime is updated when cache is updated.

--- lib/apache_lookup.rb
+++ lib/apache_lookup.rb
@@ -5,22 +5,22 @@ require 'time'
 class ApacheLookup
   VERSION = '1.0.0'
   EXPIRATION = 1000000
-  
+
   attr_accessor :cache
-  
+
   def initialize cache
     @cache = cache
   end
-  
+
   def resolve_ip ip_address
     if @cache[ip_address].nil? || Time.parse(@cache[ip_address][:mtime]) < Time.now + EXPIRATION
       @cache[ip_address] = Hash.new
       @cache[ip_address][:url] = Resolv.getname ip_address
-      @cache[ip_address][:mtime] = Time.now
+      @cache[ip_address][:mtime] = Time.now.to_s
     end
     @cache[ip_address][:url]
   end
-  
+
   def self.run
     cache = 'file'
     ApacheLookup.new cache
--- test/test_apache_lookup.rb
+++ test/test_apache_lookup.rb
@@ -5,9 +5,9 @@ class TestApacheLookup < Test::Unit::TestCase
   def setup
     @test_log = StringIO.new(File.read('./test/test_log.txt'))
     cache = YAML.load_file('test/cache.yml')
-    
+
     @apache = ApacheLookup.new cache
-    
+
     # Aliasing Resolv.getname in the test to return predictable result from any ip
     Resolv.class_eval('alias :getname_orig :getname')
     def Resolv.getname ip
@@ -15,44 +15,52 @@ class TestApacheLookup < Test::Unit::TestCase
     end
     # @test_log.each_line {|line| puts "#{@test_log.lineno}: #{line}"}
   end
-  
+
   def test_pulls_from_cache_if_in_cache_and_not_expired
     actual = @apache.resolve_ip '1.1.1.1'
-    
+
     assert_equal '1111.org', actual
   end
-  
+
   def test_resolves_ip_if_not_in_cache
     actual = @apache.resolve_ip '1.1.1.2'
-    
+
     assert_equal '1112.com', actual
   end
-  
+
   def test_resolves_ip_if_expired
     actual = @apache.resolve_ip '1.1.1.3'
-    
+
     assert_equal '1113.com', actual
   end
-  
+
   def test_caches_ip_if_expired
     @apache.resolve_ip '1.1.1.4'
     actual = @apache.cache['1.1.1.4'][:url]
-    
+
     assert_equal '1114.com', actual
   end
-  
+
   def test_caches_ip_if_not_in_cache
     @apache.resolve_ip '1.1.1.5'
     actual = @apache.cache['1.1.1.5'][:url]
-    
+
     assert_equal '1115.com', actual
   end
-  
+
+  def test_updates_mtime_when_cache_updated
+    before = Time.parse(@apache.cache['1.1.1.3'][:mtime])
+    @apache.resolve_ip '1.1.1.3'
+    after = Time.parse(@apache.cache['1.1.1.3'][:mtime])
+
+    assert before < after
+  end
+
   # def test_finds_and_stores_ips_and_lines
   #   expected = { '208.77.188.166' => [0, 4, 7], '75.119.201.189' => []}
   #   actual = ApacheLookup.gather @test_log
-  #   
-  #   
+  # 
+  # 
   #   assert_equal expected, actual
   # end
 end
