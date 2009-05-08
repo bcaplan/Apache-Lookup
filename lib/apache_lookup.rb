82afa4df88af9c96dbdc5bcf493ee1b7b81c6ccb
Brandon Caplan <bcaplan@onehub.com>
2009-05-07 20:09:13 -0700

    Setup to test cache by using an instance, fixed tests to use new method. Fixed fixture to prevent out of range times.

--- lib/apache_lookup.rb
+++ lib/apache_lookup.rb
@@ -1,6 +1,23 @@
 require 'resolv'
 require 'yaml'
+require 'time'
 
 class ApacheLookup
   VERSION = '1.0.0'
+  EXPIRATION = 1000000
+  
+  attr_accessor :cache
+  
+  def initialize cache
+    @cache = cache
+  end
+  
+  def resolve_ip ip_address
+    unless @cache[ip_address] && Time.parse(@cache[ip_address][:mtime]) < Time.now + EXPIRATION
+      @cache[ip_address][:url] = Resolv.getname ip_address
+      @cache[ip_address][:mtime] = Time.now
+    else
+      @cache[ip_address]
+    end
+  end
 end
--- test/test_apache_lookup.rb
+++ test/test_apache_lookup.rb
@@ -4,44 +4,46 @@ require "apache_lookup"
 class TestApacheLookup < Test::Unit::TestCase
   def setup
     @test_log = StringIO.new(File.read('./test/test_log.txt'))
-    @cache = YAML.load_file('test/cache.yml')
+    cache = YAML.load_file('test/cache.yml')
     
     # Aliasing Resolv.getname in the test to return predictable result from any ip
     Resolv.class_eval('alias :getname_orig :getname')
     def Resolv.getname ip
-      domain = ip.gsub('.', '') + '.com'
+      ip.gsub('.', '') + '.com'
     end
+    
+    @apache = ApacheLookup.new cache
     # @test_log.each_line {|line| puts "#{@test_log.lineno}: #{line}"}
   end
   
   def test_pulls_from_cache_if_in_cache_and_not_expired
-    actual = ApacheLookup.resolve_ip '1.1.1.1'
+    actual = @apache.resolve_ip '1.1.1.1'
     
     assert_equal '1111.org', actual
   end
   
   def test_resolves_ip_if_not_in_cache
-    actual = ApacheLookup.resolve_ip '1.1.1.2'
+    actual = @apache.resolve_ip '1.1.1.2'
     
     assert_equal '1112.com', actual
   end
   
   def test_resolves_ip_if_expired
-    actual = ApacheLookup.resolve_ip '1.1.1.3'
+    actual = @apache.resolve_ip '1.1.1.3'
     
     assert_equal '1113.com', actual
   end
   
   def test_caches_ip_if_expired
-    ApacheLookup.resolve_ip '1.1.1.4'
-    actual = @cache['1.1.1.4'][:url]
+    @apache.resolve_ip '1.1.1.4'
+    actual = @apache.cache['1.1.1.4'][:url]
     
     assert_equal '1114.com', actual
   end
   
   def test_caches_ip_if_not_in_cache
-    ApacheLookup.resolve_ip '1.1.1.5'
-    actual = @cache['1.1.1.5'][:url]
+    @apache.resolve_ip '1.1.1.5'
+    actual = @apache.cache['1.1.1.5'][:url]
     
     assert_equal '1115.com', actual
   end
